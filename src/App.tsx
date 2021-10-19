d7bd6e9f21040c713e95422e401e7398495f499a
Rayan <rayanalim@users.noreply.github.com>
2021-10-19 17:00:16 -0400

    Changed Text of front end template and addedd logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import HelloAPI from './components/HelloAPI';
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>GRIMM AUTOFINANCE</p>
-        <HelloAPI />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
