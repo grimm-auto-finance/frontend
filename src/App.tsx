f8862eba76fc63e063d56f32c81a2eced343d729
Rayan <rayanalim@users.noreply.github.com>
2021-10-19 13:01:09 -0400

    Added logo and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import logo from './grimmlogo.svg'
+import logo from './logo.png'
 import './App.css'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> GRIMM AUTO FINANCE </p>
+        <p>GRIMM AUTO FINANCE</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
