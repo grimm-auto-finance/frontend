3be3f30683b33a38c18a26832c493d1d807a8d24
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-20 19:03:00 -0400

    style: format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,54 @@
-import { useState } from 'react'
-import logo from './logo-new.png'
-import Honda from './CarImages/Honda.png'
-import Mercedes from './CarImages/Mercedes.png'
-import Ford from './CarImages/Ford.png'
-import Audi from './CarImages/Audi.png'
-import Car from './CarImages/Car.png'
-import Suzuki from './CarImages/Suzuki.png'
+import { useState } from "react";
+import logo from "./logo-new.png";
+import Honda from "./CarImages/Honda.png";
+import Mercedes from "./CarImages/Mercedes.png";
+import Ford from "./CarImages/Ford.png";
+import Audi from "./CarImages/Audi.png";
+import Car from "./CarImages/Car.png";
+import Suzuki from "./CarImages/Suzuki.png";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> GRIMM AUTO FINANCING  <br/>
+        <p>
+          {" "}
+          GRIMM AUTO FINANCING <br />
           AUTO-FINANCING MADE EASY
         </p>
 
-
-
         <form>
           <label>Credit Score : </label>
-          <input type="text" placeholder="Credit Score" name="CreditScore" required/>
+          <input
+            type="text"
+            placeholder="Credit Score"
+            name="CreditScore"
+            required
+          />
 
           <label> Budget : </label>
-          <input type="password" placeholder="Enter Budget" name="budget" required/>
+          <input
+            type="password"
+            placeholder="Enter Budget"
+            name="budget"
+            required
+          />
 
-            <label> Vehicle Model and Year : </label>
-          <input type="text" placeholder="Enter Vehicle" name="Vehicle" required/>
+          <label> Vehicle Model and Year : </label>
+          <input
+            type="text"
+            placeholder="Enter Vehicle"
+            name="Vehicle"
+            required
+          />
 
-            <button type="submit">Enter</button>
+          <button type="submit">Enter</button>
         </form>
-
 
         <p>
           <a
@@ -48,42 +62,51 @@ function App() {
         </p>
 
         <p>
-            <div class="responsive">
-                <div class="gallery">
-                    <a target="_blank" href="./CarImages/Mercedes.png">
-                        <img src={Mercedes} alt="Cinque Terre" width="512" height="512"/>
-                    </a>
-                    <div class="desc"> Mercedes SUV 2017</div>
-                </div>
-            </div>
-
-
-          <div class="responsive">
-            <div class="gallery">
-              <a target="_blank" href="./CarImages/car.png">
-                <img src={Car} alt="Car" width="512" height="512"/>
+          <div className="responsive">
+            <div className="gallery">
+              <a target="_blank" href="./CarImages/Mercedes.png">
+                <img
+                  src={Mercedes}
+                  alt="Cinque Terre"
+                  width="512"
+                  height="512"
+                />
               </a>
-              <div class="desc"> BMW something 2019</div>
+              <div className="desc"> Mercedes SUV 2017</div>
             </div>
           </div>
 
-            <div class="responsive">
-                <div class="gallery">
-                    <a target="_blank" href="./CarImages/Suzuki.png">
-                        <img src= {Honda} alt="Suzuki " width="1312" height="897"/>
-                    </a>
-                    <div class="desc">Suzuki Pattern 2020 </div>
-                </div>
+          <div className="responsive">
+            <div className="gallery">
+              <a target="_blank" href="./CarImages/car.png">
+                <img src={Car} alt="Car" width="512" height="512" />
+              </a>
+              <div className="desc"> BMW something 2019</div>
             </div>
+          </div>
 
-            <div class="responsive">
-                <div class="gallery">
-                    <a target="_blank" href="./CarImages/Ford.png">
-                        <img src= {Ford} alt="Ford Focus 2016" width="512" height="512"/>
-                    </a>
-                    <div class="desc">Ford Focus 2016</div>
-                </div>
+          <div className="responsive">
+            <div className="gallery">
+              <a target="_blank" href="./CarImages/Suzuki.png">
+                <img src={Honda} alt="Suzuki " width="1312" height="897" />
+              </a>
+              <div className="desc">Suzuki Pattern 2020 </div>
             </div>
+          </div>
+
+          <div className="responsive">
+            <div className="gallery">
+              <a target="_blank" href="./CarImages/Ford.png">
+                <img
+                  src={Ford}
+                  alt="Ford Focus 2016"
+                  width="512"
+                  height="512"
+                />
+              </a>
+              <div className="desc">Ford Focus 2016</div>
+            </div>
+          </div>
         </p>
 
         <p>
@@ -91,11 +114,9 @@ function App() {
             count is: {count}
           </button>
         </p>
-
-
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
