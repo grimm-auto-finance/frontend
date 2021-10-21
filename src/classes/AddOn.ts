071fb03c45d7d100c0966ffa8665c0cecd8003cb
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-21 11:47:29 -0400

    feat: implement AddOn class

---
+++ src/classes/AddOn.ts
@@ -0,0 +1,23 @@
+/**
+ * A class to represent an individual add-on that
+ * can be added to a car.
+ */
+export class AddOn {
+  name: string;
+  price: number | null;
+  description: string | null;
+
+  /**
+   * Constructs a new AddOn objects with the given name, price,
+   * and description.
+   * @param name
+   * @param price
+   * @param description
+   */
+  constructor(name: string, price?: number, description?: string | undefined) {
+    console.log(price);
+    this.name = name;
+    this.price = price !== undefined ? price : null;
+    this.description = description !== undefined ? description : null;
+  }
+}
