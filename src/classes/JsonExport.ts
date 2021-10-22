13367e5f3ace4300a3ca7737e3d12debd6a04d9e
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-21 23:53:51 -0400

    fix: remove JsonExport

--- src/classes/JsonExport.ts
+++
@@ -1,6 +0,0 @@
-import { Car } from "./Car";
-import { CarBuyer } from "./CarBuyer";
-
-function getCar(excar: Car, exbuyer: CarBuyer) {
-  return { car: excar.getCar(), "car buyer": exbuyer.getCarBuyer() };
-}
