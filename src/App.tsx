2066fbac24fa7cdea8b626ca20a353ca62091e2c
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-22 22:26:37 -0400

    fix: remove unnecessary wrapping p

--- src/App.tsx
+++ src/App.tsx
@@ -26,53 +26,51 @@ function App() {
 
           <UserForm />
 
-          <p>
-            <div className="responsive">
-              <div className="gallery">
-                <a target="_blank" href="./CarImages/Mercedes.png">
-                  <img
-                    src={Mercedes}
-                    alt="Cinque Terre"
-                    width="512"
-                    height="512"
-                  />
-                </a>
-                <div className="desc"> Mercedes SUV 2017</div>
-              </div>
+          <div className="responsive">
+            <div className="gallery">
+              <a target="_blank" href="./CarImages/Mercedes.png">
+                <img
+                  src={Mercedes}
+                  alt="Cinque Terre"
+                  width="512"
+                  height="512"
+                />
+              </a>
+              <div className="desc"> Mercedes SUV 2017</div>
             </div>
+          </div>
 
-            <div className="responsive">
-              <div className="gallery">
-                <a target="_blank" href="./CarImages/Car.png">
-                  <img src={Car} alt="Car" width="512" height="512" />
-                </a>
-                <div className="desc"> BMW something 2019</div>
-              </div>
+          <div className="responsive">
+            <div className="gallery">
+              <a target="_blank" href="./CarImages/Car.png">
+                <img src={Car} alt="Car" width="512" height="512" />
+              </a>
+              <div className="desc"> BMW something 2019</div>
             </div>
+          </div>
 
-            <div className="responsive">
-              <div className="gallery">
-                <a target="_blank" href="./CarImages/Suzuki.png">
-                  <img src={Honda} alt="Suzuki " width="1312" height="897" />
-                </a>
-                <div className="desc">Suzuki Pattern 2020 </div>
-              </div>
+          <div className="responsive">
+            <div className="gallery">
+              <a target="_blank" href="./CarImages/Suzuki.png">
+                <img src={Honda} alt="Suzuki " width="1312" height="897" />
+              </a>
+              <div className="desc">Suzuki Pattern 2020 </div>
             </div>
+          </div>
 
-            <div className="responsive">
-              <div className="gallery">
-                <a target="_blank" href="./CarImages/Ford.png">
-                  <img
-                    src={Ford}
-                    alt="Ford Focus 2016"
-                    width="512"
-                    height="512"
-                  />
-                </a>
-                <div className="desc">Ford Focus 2016</div>
-              </div>
+          <div className="responsive">
+            <div className="gallery">
+              <a target="_blank" href="./CarImages/Ford.png">
+                <img
+                  src={Ford}
+                  alt="Ford Focus 2016"
+                  width="512"
+                  height="512"
+                />
+              </a>
+              <div className="desc">Ford Focus 2016</div>
             </div>
-          </p>
+          </div>
         </header>
       </div>
     </div>
