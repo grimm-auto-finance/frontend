e19ffcd1e08f535894e36fb12f8e8a776f203dd4
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-11-04 11:02:44 -0400

    style: format with prettier

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,18 +4,15 @@ import AbstractLights from "../CarImages/AbstractLights.png";
 import cars3 from "../CarImages/cars3.png";
 
 const LandingPage = () => {
-    return (
+  return (
     <div class="h-40">
-        <div class="h-full">
-             <a target="_blank" href="./CarImages/AbstractLights.png">
-        <img width="100%" height="30%"
-            src={cars3}
-
-        />
-    </a>
+      <div class="h-full">
+        <a target="_blank" href="./CarImages/AbstractLights.png">
+          <img width="100%" height="30%" src={cars3} />
+        </a>
+      </div>
     </div>
-    </div>
-    );
+  );
 };
 
 export default LandingPage;
--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -25,83 +25,103 @@ const UserForm = () => {
   return (
     <div className="bg-gray-100 pb-32">
       <div>
-      <form onSubmit={handleSubmit}>
-      <Search />
+        <form onSubmit={handleSubmit}>
+          <Search />
 
-        <div className="flex items-center mb-5 inline-block">
-          <label class="inline-block w-auto mr-6 text-start"> Credit Score: </label>
-          <div className="flex-1 py-2 border-b-2 border-red-300 text-end">
-            <input
+          <div className="flex items-center mb-5 inline-block">
+            <label class="inline-block w-auto mr-6 text-start">
+              {" "}
+              Credit Score:{" "}
+            </label>
+            <div className="flex-1 py-2 border-b-2 border-red-300 text-end">
+              <input
                 id="Credit Score"
                 type="number"
                 placeholder="Credit Score"
                 name="creditScore"
-                onChange={(input) => setCreditScore(parseInt(input.target.value))}
+                onChange={(input) =>
+                  setCreditScore(parseInt(input.target.value))
+                }
                 required
-        />
-        </div>
-        </div>
-
-        <div className="flex items-center mb-5 inline-block ">
-          <label class="inline-block w-auto mr-6 text-start"> Budget: </label>
-          <div className="flex-1 py-2 border-b-2 border-red-300 text-end">
-            <input
-              id="Budget"
-              type="number"
-              placeholder="Enter Budget"
-              name="pytBudget"
-              onChange={(input) => setpytBudget(parseFloat(input.target.value))}
-              required
-          />
-        </div>
-        </div>
-
-          <div className="flex items-center mb-5 inline-block text-right">
-            <label class="inline-block w-auto mr-6 text-start"> Vehicle Make: </label>
-            <div className="flex-1 py-2 border-b-2 border-red-300">
-              <input
-                  id="Make"text-right
-                  type="text"
-                  placeholder="Enter Vehicle Make"
-                  name="vehicleMake"
-                  onChange={(input) => setMake(input.target.value)}
-                  required
               />
             </div>
           </div>
 
-        <div className="flex items-center mb-5 inline-block">
-          <label class="inline-block w-auto mr-6 text-start"> Vehicle Model: </label>
-          <div className="flex-1 py-2 border-b-2 border-red-300">
-            <input
+          <div className="flex items-center mb-5 inline-block ">
+            <label class="inline-block w-auto mr-6 text-start"> Budget: </label>
+            <div className="flex-1 py-2 border-b-2 border-red-300 text-end">
+              <input
+                id="Budget"
+                type="number"
+                placeholder="Enter Budget"
+                name="pytBudget"
+                onChange={(input) =>
+                  setpytBudget(parseFloat(input.target.value))
+                }
+                required
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center mb-5 inline-block text-right">
+            <label class="inline-block w-auto mr-6 text-start">
+              {" "}
+              Vehicle Make:{" "}
+            </label>
+            <div className="flex-1 py-2 border-b-2 border-red-300">
+              <input
+                id="Make"
+                text-right
+                type="text"
+                placeholder="Enter Vehicle Make"
+                name="vehicleMake"
+                onChange={(input) => setMake(input.target.value)}
+                required
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center mb-5 inline-block">
+            <label class="inline-block w-auto mr-6 text-start">
+              {" "}
+              Vehicle Model:{" "}
+            </label>
+            <div className="flex-1 py-2 border-b-2 border-red-300">
+              <input
                 id="Model"
                 type="text"
                 placeholder="Enter Vehicle Model"
                 name="vehicleModel"
                 onChange={(input) => setModel(input.target.value)}
                 required
-            />
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="flex items-center mb-5 inline-block">
-          <label class="inline-block w-auto mr-6 text-start"> Vehicle Year: </label>
-          <div className="flex-1 py-2 border-b-2 border-red-300">
-            <input
+          <div className="flex items-center mb-5 inline-block">
+            <label class="inline-block w-auto mr-6 text-start">
+              {" "}
+              Vehicle Year:{" "}
+            </label>
+            <div className="flex-1 py-2 border-b-2 border-red-300">
+              <input
                 id="Year"
                 type="number"
                 placeholder="Enter Vehicle Year"
                 name="vehicleYear"
                 onChange={(input) => setYear(parseInt(input.target.value))}
                 required
-            />
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="flex items-center mb-5 inline-block">
-          <label class="inline-block w-auto mr-6 text-start"> Vehicle Distance Driven (KMs): </label>
-          <div className="flex-1 py-2 border-b-2 border-red-300">
-            <input
+          <div className="flex items-center mb-5 inline-block">
+            <label class="inline-block w-auto mr-6 text-start">
+              {" "}
+              Vehicle Distance Driven (KMs):{" "}
+            </label>
+            <div className="flex-1 py-2 border-b-2 border-red-300">
+              <input
                 id="Distance Driven"
                 type="number"
                 step={0.01}
@@ -109,14 +129,16 @@ const UserForm = () => {
                 name="vehicleKms"
                 onChange={(input) => setKms(parseFloat(input.target.value))}
                 required
-            />
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="flex items-center mb-5 inline-block pb-8">
-          <label class="inline-block w-auto mr-6 text-start">Vehicle Price: </label>
-          <div className="flex-1 py-2 border-b-2 border-red-300">
-            <input
+          <div className="flex items-center mb-5 inline-block pb-8">
+            <label class="inline-block w-auto mr-6 text-start">
+              Vehicle Price:{" "}
+            </label>
+            <div className="flex-1 py-2 border-b-2 border-red-300">
+              <input
                 id="Price"
                 type="number"
                 step={0.01}
@@ -124,15 +146,19 @@ const UserForm = () => {
                 name="vehiclePrice"
                 onChange={(input) => setPrice(parseFloat(input.target.value))}
                 required
-            />
+              />
+            </div>
           </div>
-        </div>
 
-        <button type="submit" class="bg-blue-200 text-3xl text-gray-400 rounded-lg text-center py-8 px-32">Enter</button>
-      </form>
+          <button
+            type="submit"
+            class="bg-blue-200 text-3xl text-gray-400 rounded-lg text-center py-8 px-32"
+          >
+            Enter
+          </button>
+        </form>
       </div>
     </div>
-
   );
 };
 
