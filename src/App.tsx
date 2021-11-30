43f9aef3310e774392c52af6dae47eaabb4a2af1
ianlavine <71903686+ianlavine@users.noreply.github.com>
2021-11-29 19:25:11 -0500

    refactor: dashboard added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import "tailwindcss/tailwind.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Autofill from "./scenes/autofill"
+import DashBoard from "./scenes/dashboard"
 
 function App() {
   return (
