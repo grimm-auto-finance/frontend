317b12e6f9d8abeaae9bf3f40f83a7f2c3e29880
ianlavine <71903686+ianlavine@users.noreply.github.com>
2021-11-29 19:19:04 -0500

    autofill moved to scenes

---
+++ src/scenes/autofill.tsx
@@ -0,0 +1,29 @@
+import logo from "./logo-new.png";
+import "./App.css";
+import Navbar from "../components/Navbar";
+import LandingPage from "../components/LandingPage";
+import Footer from "../components/Footer";
+import Search from "../components/Search";
+import "tailwindcss/tailwind.css";
+
+const Autofill = () => {
+  return (
+    <div className="App">
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <LandingPage />
+      </div>
+      <div className="content">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Search />
+        </header>
+        <Footer />
+      </div> 
+    </div>
+  );
+}
+
+export default Autofill;
