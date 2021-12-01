64f878bc12dd84c15aa6743e74aa7962e7a297c4
Rayan <rayanalim@users.noreply.github.com>
2021-12-01 16:11:12 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         🚘 GRIMM AUTO-FINANCE
       </div>
         <div className="mt-8">
-            <label for="toggle" className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+            <label htmlFor="toggle" className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
                 <input id="toggle" type="checkbox" className="toggle-checkbox absolute block w-4 h-4 mt-1 mx-1 rounded-full bg-white border-none appearance-none cursor-pointer outline-none focus:outline-none transition-all" x-model="darkMode" value="1"/>
                 <span className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-900 bg-opacity-20 cursor-pointer focus:outline-none transition-all"></span>
             </label>
@@ -14,4 +14,5 @@ const Navbar = () => {
   );
 };
 
+// @ts-ignore
 export default Navbar;
