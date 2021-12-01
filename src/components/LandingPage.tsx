e4ae07c7d8a5cf5ab4b958cc954a7bc8e9c20244
Rayan <rayanalim@users.noreply.github.com>
2021-12-01 15:19:51 -0500

    Add theming

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -5,8 +5,8 @@ import cars3 from "../car-images/cars3.png";
 
 const LandingPage = () => {
   return (
-    <div className="h-40">
-      <div className="h-full">
+    <div className="h-auto">
+      <div className="h-2">
         <a target="_blank" href="./CarImages/AbstractLights.png">
           <img width="100%" height="30%" src={cars3} />
         </a>
