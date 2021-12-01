4a03c56e2eaefc80683f0f7a0eb29ef4b4c7f3f5
Rayan <rayanalim@users.noreply.github.com>
2021-12-01 15:20:13 -0500

    Toggle in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,12 @@ const Navbar = () => {
       <div className="text-5xl text-gray-600 font-semibold py-1">
         🚘 GRIMM AUTO-FINANCE
       </div>
+        <div className="mt-8">
+            <label for="toggle" className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+                <input id="toggle" type="checkbox" className="toggle-checkbox absolute block w-4 h-4 mt-1 mx-1 rounded-full bg-white border-none appearance-none cursor-pointer outline-none focus:outline-none transition-all" x-model="darkMode" value="1"/>
+                <span className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-900 bg-opacity-20 cursor-pointer focus:outline-none transition-all"></span>
+            </label>
+        </div>
     </nav>
   );
 };
