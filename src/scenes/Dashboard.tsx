bea7ab20c5350c402b30677b6b1db38f2ea622c6
Rayan <rayanalim@users.noreply.github.com>
2021-12-01 15:21:26 -0500

    Update Dashboard.tsx

--- src/scenes/Dashboard.tsx
+++ src/scenes/Dashboard.tsx
@@ -68,6 +68,81 @@ function Dashboard() {
             })
           : []}
       </div>
+
+      <div className="w-full md:w-3/5 mx-auto p-8">
+        <p>
+          Open <strong>multiple</strong>
+        </p>
+        <div className="shadow-md">
+          <div className="tab w-full overflow-hidden border-t">
+            <input
+              className="absolute opacity-0 "
+              id="tab-multi-one"
+              type="checkbox"
+              name="tabs"
+            />
+            <label
+              className="block p-5 leading-normal cursor-pointer"
+              for="tab-multi-one"
+            >
+              Label One
+            </label>
+            <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+              <p className="p-5">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Tenetur, architecto, explicabo perferendis nostrum, maxime
+                impedit atque odit sunt pariatur illo obcaecati soluta molestias
+                iure facere dolorum adipisci eum? Saepe, itaque.
+              </p>
+            </div>
+          </div>
+          <div className="tab w-full overflow-hidden border-t">
+            <input
+              className="absolute opacity-0"
+              id="tab-multi-two"
+              type="checkbox"
+              name="tabs"
+            />
+            <label
+              className="block p-5 leading-normal cursor-pointer"
+              for="tab-multi-two"
+            >
+              Label Two
+            </label>
+            <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+              <p className="p-5">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Tenetur, architecto, explicabo perferendis nostrum, maxime
+                impedit atque odit sunt pariatur illo obcaecati soluta molestias
+                iure facere dolorum adipisci eum? Saepe, itaque.
+              </p>
+            </div>
+          </div>
+          <div className="tab w-full overflow-hidden border-t">
+            <input
+              className="absolute opacity-0"
+              id="tab-multi-three"
+              type="checkbox"
+              name="tabs"
+            />
+            <label
+              className="block p-5 leading-normal cursor-pointer"
+              for="tab-multi-three"
+            >
+              Label Three
+            </label>
+            <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal">
+              <p className="p-5">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Tenetur, architecto, explicabo perferendis nostrum, maxime
+                impedit atque odit sunt pariatur illo obcaecati soluta molestias
+                iure facere dolorum adipisci eum? Saepe, itaque.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="flex-grow bg-white flex flex-col">
         <div className="bg-red-300 shadow-lg h-16 flex justify-between">
           <Link
