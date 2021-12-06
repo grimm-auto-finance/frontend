1042c3aece7492797e11c1cbb1a0c53e294ee793
Rayan <rayanalim@users.noreply.github.com>
2021-12-06 06:18:16 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,27 @@
+import logo from "../logo-new.png";
+
 const Navbar = () => {
   return (
-    <nav className="flex mx-auto bg-white px-20 pt-10 pb-10 text-gray-700 dark:bg-gray-700 dark:text-red-200">
-      <div className="text-5xl text-gray-600 font-semibold py-1">
-        🚘 GRIMM AUTO-FINANCE
+    <nav className="justify-center">
+        <div className="text-center pb-4 md:pb-16">
+        <h1
+          className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+          data-aos="zoom-y-out"
+        >
+            <img
+                className="inline-block rounded-full"
+                src={logo}
+                width="180"
+                height="150"
+            />  GRIMM{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-indigo-200">
+            {" "}
+            AUTOFINANCE
+          </span>
+            
+            
+        </h1>
+
       </div>
     </nav>
   );
