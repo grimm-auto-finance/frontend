9f15dc6986a924d3bcffd89c54f8510c90ee79d0
Rayan <rayanalim@users.noreply.github.com>
2021-12-06 03:04:43 -0500

    Reformat Addons search

--- src/components/AddOnBox.tsx
+++ src/components/AddOnBox.tsx
@@ -14,39 +14,48 @@ function AddOnBox(props: {
     "transition fill-current w-6 inline-block" + (expanded ? " rotate-90" : "");
 
   return (
-    <div className="w-full text-blueGray-800 border-2 border-blueGray-800 rounded-lg p-1 text-left">
-      <div className="flex gap-2 ">
-        <button
-          className="h-full hover:drop-shadow-2xl transition hover:opacity-80"
-          onClick={() => setExpanded(!expanded)}
-        >
-          <svg viewBox="0 0 24 24" className={chevronClassName}>
-            <path d={mdiChevronRight} />
-          </svg>
-        </button>
-        <div className="truncate">{props.addOn.name}</div>
-        <div className="flex-grow" />
-        <div>${props.addOn.price}</div>
-        {/* TODO: Improve wrapping when size is small here */}
-        <button
-          className="bg-blue-300 rounded-3xl shadow-xl hover:shadow-2xl transition h-auto text-sm text-blue-900 hover:opacity-100 px-2 whitespace-nowrap"
-          onClick={() => {
-            if (added) {
-              props.onRemove(props.addOn);
-            } else {
-              props.onAdd(props.addOn);
-            }
-            setAdded(!added);
-          }}
-        >
-          <svg
-            viewBox="0 0 24 24"
-            className="text-white fill-current hover:drop-shadow-2xl transition inline-block w-4 text-current"
+    <div
+      className="shadow-md tab w-full
+     overflow-y-auto border-t w-full
+     border-0 border-t-8 hover:border-blue-800
+     rounded-sm p-8 text-left"
+    >
+      <div className="">
+        <div className="flex gap-2 ">
+          <button
+            className="h-full hover:drop-shadow-2xl transition hover:opacity-80"
+            onClick={() => setExpanded(!expanded)}
           >
-            <path d={added ? mdiMinusCircle : mdiPlusCircle} />
-          </svg>
-          <span className="inline-block">{added ? "Remove" : "Add"}</span>
-        </button>
+            <svg viewBox="0 0 24 24" className={chevronClassName}>
+              <path d={mdiChevronRight} />
+            </svg>
+          </button>
+
+          <div className="truncate flex-grow">{props.addOn.name}</div>
+          <div className="flex-grow" />
+          <div>${props.addOn.price}</div>
+          {/* TODO: Improve wrapping when size is small here */}
+
+          <button
+            className="bg-blue-500 pl-2 inline-block rounded-3xl shadow-xl hover:shadow-2xl transition h-auto text-sm hover:opacity-100 px-2 whitespace-nowrap"
+            onClick={() => {
+              if (added) {
+                props.onRemove(props.addOn);
+              } else {
+                props.onAdd(props.addOn);
+              }
+              setAdded(!added);
+            }}
+          >
+            <svg
+              viewBox="0 0 24 24"
+              className="text-white fill-current hover:drop-shadow-2xl transition inline-block w-4 text-current"
+            >
+              <path d={added ? mdiMinusCircle : mdiPlusCircle} />
+            </svg>
+            <span className="inline-block">{added ? "Remove" : "Add"}</span>
+          </button>
+        </div>
       </div>
       {expanded && props.addOn.description}
     </div>
