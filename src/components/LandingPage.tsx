32bdc661fff1b3942ffea80003fad69ad3a46e2c
Rayan <rayanalim@users.noreply.github.com>
2021-12-06 12:37:54 -0500

    prettier

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -5,7 +5,7 @@ import cars3 from "../car-images/cars3.png";
 const LandingPage = () => {
   return (
     <div className="h-64 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
-          <img src={cars3} />
+      <img src={cars3} />
     </div>
   );
 };
