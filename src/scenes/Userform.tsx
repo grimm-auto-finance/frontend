f3e41707d509b2c42754e22772924cc8b0df16db
Rayan <rayanalim@users.noreply.github.com>
2021-12-06 06:18:19 -0500

    Update Userform.tsx

--- src/scenes/Userform.tsx
+++ src/scenes/Userform.tsx
@@ -5,23 +5,36 @@ import LandingPage from "../components/LandingPage";
 import Footer from "../components/Footer";
 import Search from "../components/Search";
 import "tailwindcss/tailwind.css";
+import { useState } from "react";
 
 const Userform = () => {
+  const [mode, setMode] = useState(false);
+
+  const handleClick = () => {
+    setMode(!mode);
+  };
   return (
-    <div className="App">
-      <div>
-        <Navbar />
-      </div>
-      <div>
-        <LandingPage />
-      </div>
-      <div className="content">
-        <header className="justify-center App-header">
-          <img src={logo} className="h-32" alt="logo" />
-        </header>
+    <div>
+      <div className="text-center">
+
+          <div
+          className={
+            mode
+              ? "dark:bg-gray-800 w-auto p-6 shadow-xl transition duration-700 dark:text-white transition duration-700"
+              : "bg-gray-100 shadow-xl  mb-5 w-auto  p-6 transition duration-700 text-gray-600 transition duration-700"
+          }
+        >
+          <button
+            className="inline-block rounded-lg overflow-x-auto h-8"
+            onClick={handleClick}
+          >
+            {mode ? "🌙" : "☀️"}
+          </button>
+          <Navbar />
           <Search />
-        <Footer />
+        </div>
       </div>
+        <Footer />
     </div>
   );
 };
