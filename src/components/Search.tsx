f27d7e1246c1f7a30eb040c29060714c93e3dc88
Mogtaba-Alim <alim.mogtaba@gmail.com>
2021-12-07 01:41:33 -0500

    fix: format with prettier

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -27,8 +27,8 @@ const Search = () => {
     <div className="justify-center pt-4">
       <form className=" w-auto">
         <script type="text/javascript" src="Search.tsx">
-                setCreditScore(getSimulatedCreditScore())
-              </script>
+          setCreditScore(getSimulatedCreditScore())
+        </script>
 
         <div className="flex justify-center rounded border-0 border-t-4 border-b-4 hover:border-blue-800 mb-8 m-4 h-auto">
           <div className="flex items-center inline bg-gray-200 py-2 px-4 text-gray-600 select-none">
